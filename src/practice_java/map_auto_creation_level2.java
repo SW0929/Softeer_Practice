83914028f6a4d4238d9da536c863784ab98f9ded
sw <sjugas1234@naver.com>
2023-05-27 21:23:26 +0900

    map_auto_creation

---
+++ src/practice_java/map_auto_creation_level2.java
@@ -0,0 +1,18 @@
+package practice_java;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class map_auto_creation_level2 {
+    public static void main(String args[]) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        int init = 2;
+        for (int i = 0; i < N; i++){
+            init = init + (init-1);
+        }
+        
+        System.out.println(init*init);
+    }
+}
